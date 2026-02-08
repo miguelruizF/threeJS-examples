fe9a392b90a09221dd14129c334d760a36ceecc9
miguelruizF-HP <later481@gmail.com>
2026-02-08 14:57:34 -0600

    OrbitControl import change

--- src/scene.js
+++ src/scene.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import getLayer from "../asset/getLayer.js";
-import { OrbitControls } from "../node_modules/three/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 
 const scene = new THREE.Scene();
